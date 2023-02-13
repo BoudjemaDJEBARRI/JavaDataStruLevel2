155c4b2bf3f0f9c4ce0af4b1b18456a0067e1d7b
Boudjema DJEBARRI <boudjema.djebari@gmail.com>
2023-02-13 17:35:34 +0100

    Fibonacci

---
+++ src/CountDuplicateChar.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class CountDuplicateChar {
+
+    public static void main(String[] args) {
+        // Input Text
+        System.out.println("Enter Sentence : ");
+        Scanner scan2 = new Scanner(System.in);
+        String text1 = scan2.nextLine();
+
+        // Text to character array
+        char[] char1 = text1.toCharArray();
+
+        Map<Character, Integer> charMap1 = new HashMap<>();
+        for (Character ch1 : char1) {
+            if (charMap1.containsKey(ch1)){
+                charMap1.put(ch1, charMap1.get(ch1) + 1);
+            } else {
+                charMap1.put(ch1, 1);
+            }
+        }
+
+        Set<Character> keys1 = charMap1.keySet();
+        for (Character ch1 : keys1) {
+            if (charMap1.get(ch1) >= 1){
+                System.out.println("Character : " + ch1 + " --> " + charMap1.get(ch1) + " times");
+            }
+
+        }
+    }
+}
